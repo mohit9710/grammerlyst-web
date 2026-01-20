b61e767ddc2f071dd8b3130a273f048b5edbc64b
mohit9710 <newxyz8@gmail.com>
2026-01-20 20:33:26 +0530

    build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 // import "../styles/auth.css";
+import type { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
       <head>
